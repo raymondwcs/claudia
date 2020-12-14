465a6331f26674e6a7e92ab4e4bfa75c1a34f40a
Raymond SO <raymond.wc.so@gmail.com>
2020-12-14 21:52:23 +0800

    no longer using querystring parameters

--- shape/app.js
+++ shape/app.js
@@ -11,26 +11,26 @@ var api = new ApiBuilder()
 
 module.exports = api;
 
-api.get('/shape/square/perimeter', (req) => {
+api.get('/shape/square/perimeter/{length}', (req) => {
     let result = {}
-    result['perimeter'] = req.queryString.length * 4
+    result['perimeter'] = req.pathParams.length * 4
     return result
 })
 
-api.get('/shape/square/area', (req) => {
+api.get('/shape/square/area/{length}', (req) => {
     let result = {}
-    result['area'] = Math.pow(req.queryString.length,2)
+    result['area'] = Math.pow(req.pathParams.length,2)
     return result
 })
 
-api.get('/shape/circle/perimeter', (req) => {
+api.get('/shape/circle/perimeter/{radius}', (req) => {
     let result = {}
-    result['perimeter'] = 2 * Math.PI * req.queryString.radius
+    result['perimeter'] = 2 * Math.PI * req.pathParams.radius
     return result
 })
 
-api.get('/shape/circle/area', (req) => {
+api.get('/shape/circle/area/{radius}', (req) => {
     let result = {}
-    result['area'] = Math.PI * Math.pow(req.queryString.radius,2)
+    result['area'] = Math.PI * Math.pow(req.pathParams.radius,2)
     return result
 })
